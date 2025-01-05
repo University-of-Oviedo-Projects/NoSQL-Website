b6f218d8f099076b0e507a5d0998a8c83115ecde
adrianmfuentes <amf13azul@proton.me>
2025-01-05 19:22:23 +0100

    It should be uploaded to render, not vercel, xD

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-const port = 3000;
+const port = process.env.PORT || 3000; 
 const app = express();
 
 const corsOptions = {
